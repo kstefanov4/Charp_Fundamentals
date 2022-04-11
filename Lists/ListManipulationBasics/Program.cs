66cfe4fcf1124596b50428482cbab346e8113bb7
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-11 23:24:45 +0300

    C#

---
+++ Lists/ListManipulationBasics/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListManipulationBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numList = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                string[] inputArray = input.Split();
+                string command = inputArray[0];
+                int number = int.Parse(inputArray[1]);
+
+                switch (command)
+                {
+                    case "Add":
+                        numList.Add(number);
+                        break;
+                    case "Remove":
+                        numList.Remove(number);
+                        break;
+                    case "RemoveAt":
+                        numList.RemoveAt(number);
+                        break;
+                    case "Insert":
+                        int index = int.Parse(inputArray[2]);
+                        numList.Insert(index, number);
+                        break;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(" ", numList));
+        }
+    }
+}
---
+++ Lists/Train/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Train
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagonList = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int maxCapacity = int.Parse(Console.ReadLine());
+
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                string[] inputArray = input.Split();
+
+                if (inputArray.Length > 1)
+                {
+                    wagonList.Add(int.Parse(inputArray[1]));
+                }
+                else
+                {
+                    int passengers = int.Parse(inputArray[0]);
+                    for (int i = 0; i < wagonList.Count; i++)
+                    {
+                        if (wagonList[i] + passengers <= maxCapacity)
+                        {
+                            wagonList[i] = wagonList[i] + passengers;
+                            break;
+                        }
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(" ", wagonList));
+
+        }
+    }
+}
