c4397ff850f77dd7e4933a1e0b5751b4fdbe128d
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-11 23:12:22 +0300

    c#

---
+++ Lists/T01.SumAdjacentEqualNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T01.SumAdjacentEqualNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numList = Console.ReadLine().Split().Select(double.Parse).ToList();
+
+            for (int i = 0; i < numList.Count - 1; i++)
+            {
+                if (numList[i] == numList[i + 1])
+                {
+                    double sum = numList[i] + numList[i + 1];
+                    numList.RemoveAt(i + 1);
+                    numList.RemoveAt(i);
+                    numList.Insert(i, sum);
+                    i = -1;
+
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numList));
+        }
+    }
+}
