81f2cba84d35adff8c4d1c31164a8f53715cc4af
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-11 23:16:12 +0300

    c#

---
+++ Lists/GaussTrick/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GaussTrick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numList = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int lenght = numList.Count / 2;
+
+            for (int i = 0; i < lenght; i++)
+            {
+                int sum = numList[i] + numList[numList.Count - 1];
+                numList.RemoveAt(i);
+                numList.Insert(i, sum);
+                numList.RemoveAt(numList.Count - 1);
+            }
+
+            Console.WriteLine(string.Join(" ", numList));
+        }
+    }
+}
