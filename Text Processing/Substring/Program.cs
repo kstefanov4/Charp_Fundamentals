7189c74e18055e593f55e4817ca1c5bfb289d432
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-12 13:09:15 +0300

    C#

---
+++ Text Processing/Substring/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string keyWord = Console.ReadLine();
+            string word = Console.ReadLine();
+
+            int indexOf = 0;
+            int wordLenght = keyWord.Length;
+
+            while (indexOf != -1)
+            {
+                indexOf = word.IndexOf(keyWord);
+                if (indexOf == -1)
+                {
+                    break;
+                }
+                word = word.Remove(indexOf, wordLenght);
+            }
+            Console.WriteLine(word);
+        }
+    }
+}
