2ccd061a3bba683ca1c7d6348a35569630dd6921
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-12 15:45:27 +0300

    C#

---
+++ Text Processing/ExtractFile/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExtractFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputArray = Console.ReadLine().Split("\\");
+
+            string word = inputArray[inputArray.Length - 1];
+            string[] lastWord = word.Split('.');
+            string fileName = lastWord[0];
+            string extension = lastWord[1];
+
+            Console.WriteLine("File name: " + fileName);
+            Console.WriteLine("File extension: " + extension);
+        }
+    }
+}
