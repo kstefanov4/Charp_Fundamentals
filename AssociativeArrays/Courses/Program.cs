baee767b7bd8406f38aa0abf5b6d0cf45420b46a
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-12 10:19:08 +0300

    C#

---
+++ AssociativeArrays/Courses/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+
+namespace Courses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Dictionary<string, List<string>> courseMap = new Dictionary<string, List<string>>();
+
+            while (input != "end")
+            {
+                string course = input.Split(" : ")[0];
+                string student = input.Split(" : ")[1];
+
+                if (!courseMap.ContainsKey(course))
+                {
+                    courseMap.Add(course, new List<string>());
+                }
+                courseMap[course].Add(student);
+
+                input = Console.ReadLine();
+
+            }
+
+            foreach (var course in courseMap.OrderByDescending(x => x.Value.Count()))
+            {
+                Console.WriteLine($"{course.Key}: {course.Value.Count()}");
+                foreach (var name in course.Value.OrderBy(n => n))
+                {
+                    Console.WriteLine($"-- {name}");
+                }
+            }
+        }
+    }
+}
