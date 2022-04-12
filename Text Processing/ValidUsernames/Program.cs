cce035b37b7b419558ca88dec3e8389077b2e567
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-12 13:44:03 +0300

    C#

---
+++ Text Processing/ValidUsernames/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ValidUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputArray = Console.ReadLine().Split(", ");
+
+            foreach (var input in inputArray)
+            {
+                if (isValid(input))
+                {
+                    Console.WriteLine(input);
+                }
+            }
+        }
+
+        private static bool isValid(string input)
+        {
+            if (input.Length < 3 || input.Length > 16)
+            {
+                return false;
+            }
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentChar = input[i];
+                if (!(char.IsLetterOrDigit(currentChar) || currentChar == '-' || currentChar == '_'))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
