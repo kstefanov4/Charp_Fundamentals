84efee6b6b3f2cc9af14af96ba1075033c7de278
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-12 10:47:07 +0300

    C#

---
+++ AssociativeArrays/CompanyUsers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CompanyUsers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Dictionary<string, List<string>> userMap = new Dictionary<string, List<string>>();
+
+            while (input != "End")
+            {
+                string company = input.Split(" -> ")[0];
+                string userID = input.Split(" -> ")[1];
+
+                if (!userMap.ContainsKey(company))
+                {
+                    userMap.Add(company,new List<string>());
+
+                }
+                if (!userMap[company].Contains(userID))
+                {
+                    userMap[company].Add(userID);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var user in userMap)
+            {
+                Console.WriteLine(user.Key);
+                foreach (var item in user.Value)
+                {
+                    Console.WriteLine($"-- {item}");
+                }
+            }
+        }
+    }
+}
