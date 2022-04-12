94c9ba4e0e84942243bc8cb3b62df8468fa217d3
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-12 13:18:18 +0300

    C#

---
+++ Text Processing/TextFilter/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace TextFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannerWordsArray = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+
+            foreach (var word in bannerWordsArray)
+            {
+                string cryptedWord = getCryptedWord(word);
+                text = text.Replace(word, cryptedWord);
+            }
+            Console.WriteLine(text);
+        }
+
+        private static string getCryptedWord(string word)
+        {
+            string banWord = "";
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                sb.Append("*");
+            }
+            banWord = sb.ToString();
+            return banWord;
+        }
+    }
+}
