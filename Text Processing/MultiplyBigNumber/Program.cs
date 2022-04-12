28774eef9bf3119943216f230b0beb3b00cc343e
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-12 16:05:23 +0300

    C#

---
+++ Text Processing/MultiplyBigNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Numerics;
+using System.Linq;
+
+namespace MultiplyBigNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger firstNum = BigInteger.Parse(Console.ReadLine());
+            BigInteger secondNum = BigInteger.Parse(Console.ReadLine());
+            firstNum = firstNum * secondNum;
+
+            Console.WriteLine(firstNum);
+        }
+    }
+}
