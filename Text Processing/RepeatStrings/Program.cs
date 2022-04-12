cfe498c86056c7d1ab1e93aa6ba941af1df7ff58
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-12 12:53:27 +0300

    C#

---
+++ Text Processing/RepeatStrings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RepeatStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputArray = Console.ReadLine().Split();
+
+            foreach (var word in inputArray)
+            {
+                printWord(word);
+            }
+        }
+
+        private static void printWord(string word)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                Console.Write(word);
+            }
+        }
+    }
+}
---
+++ Text Processing/ReverseStrings/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                char[] charArray = input.ToCharArray();
+                List<char> charList = new List<char>();
+
+                foreach (var char1 in charArray)
+                {
+                    charList.Add(char1);
+                }
+
+                charList.Reverse();
+                StringBuilder sb = new StringBuilder();
+                foreach (var reversedChar in charList)
+                {
+                    sb.Append(reversedChar);
+                }
+                string reverseWord = sb.ToString();
+                Console.WriteLine(input + " = " + reverseWord);
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
