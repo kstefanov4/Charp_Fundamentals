9c5a3873e99e8b5113247539aae7aa1d0a029546
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-12 16:11:36 +0300

    C#

---
+++ Text Processing/ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ReplaceRepeatingChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] inputArray = Console.ReadLine().ToCharArray();
+            Console.Write(inputArray[0]);
+
+            for (int i = 1; i < inputArray.Length; i++)
+            {
+                if (inputArray[i] != inputArray[i-1])
+                {
+                    Console.Write(inputArray[i]);
+                }
+            }
+        }
+    }
+}
