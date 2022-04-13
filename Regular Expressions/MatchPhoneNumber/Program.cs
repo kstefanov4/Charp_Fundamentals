34037a78aa5156800b00d743802941a52aaca193
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-13 09:05:09 +0300

    C#

---
+++ Regular Expressions/MatchPhoneNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace MatchPhoneNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = @"\+359([- ])2\1[\d]{3}\1[\d]{4}\b";
+            string input = Console.ReadLine();
+
+            MatchCollection matchedNumbers = Regex.Matches(input, regex);
+
+            var matched = matchedNumbers.Cast<Match>().Select(a => a.Value.Trim()).ToArray();
+
+            Console.WriteLine(string.Join(", ",matched));
+        }
+    }
+}
