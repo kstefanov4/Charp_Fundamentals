f45db7a87c5c59cbf25e1a4df0c4f02eb9333852
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-13 08:56:03 +0300

    C#

---
+++ Regular Expressions/MatchFullName/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MatchFullName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+            string names = Console.ReadLine();
+
+            MatchCollection matchedNames = Regex.Matches(names, regex);
+
+            foreach (Match name in matchedNames)
+            {
+                Console.WriteLine(name.Value + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
