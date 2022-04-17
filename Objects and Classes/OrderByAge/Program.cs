050dc2d236ab655469144c9dfb6008b8334cad4b
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-18 00:46:13 +0300

    C#

---
+++ Objects and Classes/OrderByAge/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace OrderByAge
+{
+    class Person
+    {
+        public Person(string name, string id, int age)
+        {
+            Name = name;
+            ID = id;
+            Age = age;
+        }
+        public string Name { get; set; }
+        public string ID { get; set; }
+        public int Age { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            List<Person> personList = new List<Person>();
+            while (input != "End")
+            {
+                string name = input.Split()[0];
+                string id = input.Split()[1];
+                int age = int.Parse(input.Split()[2]);
+
+                Person person = new Person(name, id, age);
+                if (personList.Any(x=>x.ID == id))
+                {
+                    personList.Where(x => x.ID == id).First().Age = age;
+                    personList.Where(x => x.ID == id).First().Name = name;
+                }
+                else
+                {
+                    personList.Add(person);                
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var people in personList.OrderBy(x=>x.Age))
+            {
+                Console.WriteLine($"{people.Name} with ID: {people.ID} is {people.Age} years old.");
+            }
+        }
+    }
+}
